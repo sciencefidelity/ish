4cf5b8314bf1fdf38a9edb3b2fd303abd0cf4b5c
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-01-31 18:37:05 +0000

    Update ish-build.sh

--- ish-build.sh
+++ ish-build.sh
@@ -19,8 +19,8 @@ chmod 777 /dev/null
 # Build the dotfiles
 
 wget https://raw.githubusercontent.com/sciencefidelity/ish/main/.profile
-wget https://github.com/sciencefidelity/dotfiles/blob/main/.bash_profile
-wget https://github.com/sciencefidelity/dotfiles/blob/main/.vimrc
+wget https://raw.githubusercontent.com/sciencefidelity/dotfiles/main/.bash_profile
+wget https://raw.githubusercontent.com/sciencefidelity/dotfiles/main/.vimrc
 
 mkdir .vim
 mkdir .vim/colors
@@ -28,10 +28,12 @@ mkdir .vim/colors
 curl https://raw.githubusercontent.com/lifepillar/vim-gruvbox8/master/colors/gruvbox8.vim -o .vim/colors/gruvbox8.vim
 curl https://raw.githubusercontent.com/lifepillar/vim-wwdc16-theme/master/colors/wwdc16.vim -o .vim/colors/wwdc16.vim
 
+rm /etc/motd
+curl https://raw.githubusercontent.com/sciencefidelity/ish/main/motd -o /etc/motd
+
 # Set up networking
 
 rm /etc/init.d/networking
-
 curl https://raw.githubusercontent.com/sciencefidelity/ish/main/networking -o /etc/init.d/networking
 
 # Set up Apache2
